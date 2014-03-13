653d478634d682a396ba6abc061d4fbaaea80267
Shravan Pabba <skpabba@gmail.com>
2014-03-13 18:32:11 -0400

    Changes to make Parquet writer work

---
+++ runSparkWriteParquet.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export SPARK_HOME=/opt/cloudera/parcels/CDH/lib/spark
+export SPARK_LIB=$SPARK_HOME/lib
+
+export DRIVER_CLASSPATH=/home/cloudera/SparkDemo.jar
+# This is comma separated list unlike CLASSPATH's which are semi colon separated
+export DRIVER_JARS_LIST=/home/cloudera/SparkDemo.jar
+export CLASSPATH=/opt/cloudera/parcels/CDH-5.0.0-0.cdh5b2.p0.27/lib/spark/conf:/opt/cloudera/parcels/CDH-5.0.0-0.cdh5b2.p0.27/lib/spark/core/lib/*:/opt/cloudera/parcels/CDH-5.0.0-0.cdh5b2.p0.27/lib/spark/repl/lib/*:/opt/cloudera/parcels/CDH-5.0.0-0.cdh5b2.p0.27/lib/spark/examples/lib/*:/opt/cloudera/parcels/CDH-5.0.0-0.cdh5b2.p0.27/lib/spark/bagel/lib/*:/opt/cloudera/parcels/CDH-5.0.0-0.cdh5b2.p0.27/lib/spark/mllib/lib/*:/opt/cloudera/parcels/CDH-5.0.0-0.cdh5b2.p0.27/lib/spark/streaming/lib/*:/opt/cloudera/parcels/CDH-5.0.0-0.cdh5b2.p0.27/lib/spark/lib/*:/etc/hadoop/conf:/opt/cloudera/parcels/CDH-5.0.0-0.cdh5b2.p0.27/lib/hadoop/*:/opt/cloudera/parcels/CDH-5.0.0-0.cdh5b2.p0.27/lib/hadoop/../hadoop-hdfs/*:/opt/cloudera/parcels/CDH-5.0.0-0.cdh5b2.p0.27/lib/hadoop/../hadoop-yarn/*:/opt/cloudera/parcels/CDH-5.0.0-0.cdh5b2.p0.27/lib/hadoop/../hadoop-mapreduce/*:/opt/cloudera/parcels/CDH-5.0.0-0.cdh5b2.p0.27/lib/spark/lib/scala-library.jar:/opt/cloudera/parcels/CDH-5.0.0-0.cdh5b2.p0.27/lib/spark/lib/scala-compiler.jar:/opt/cloudera/parcels/CDH-5.0.0-0.cdh5b2.p0.27/lib/spark/lib/jline.jar:$DRIVER_CLASSPATH
+
+java com.cloudera.spark.SparkWriteParquet $DRIVER_JARS_LIST spark://clouderavm.localdomain:7077 hdfs://clouderavm.localdomain:8020/user/cloudera/events/events.csv hdfs://clouderavm.localdomain:8020/user/cloudera/events/parquet/
--- src/main/java/com/cloudera/spark/SparkWriteParquet.java
+++ src/main/java/com/cloudera/spark/SparkWriteParquet.java
@@ -2,16 +2,11 @@ package com.cloudera.spark;
 
 import com.cloudera.spark.common.STBEvent;
 import com.cloudera.spark.util.EventParser;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.SparkHadoopWriter;
-import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.PairFunction;
 import parquet.avro.AvroParquetOutputFormat;
 import parquet.avro.AvroWriteSupport;
@@ -20,7 +15,6 @@ import parquet.hadoop.metadata.CompressionCodecName;
 import scala.Tuple2;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -49,7 +43,6 @@ public class SparkWriteParquet {
         SparkConf conf = new SparkConf();
         conf.setMaster(master).setAppName("parquetWriter");
         conf.setJars(new String[]{jarsList});
-//        conf.setJars(new String[]{"/home/cloudera/SparkDemo.jar"});
 
         JavaSparkContext sc = new JavaSparkContext(conf);
 
@@ -60,30 +53,25 @@ public class SparkWriteParquet {
 
 //        System.in.read();
 
+        final Void v = null;
+
         try {
             // Configure the ParquetOutputFormat to use Avro as the serialization format
             ParquetOutputFormat.setWriteSupportClass(job, AvroWriteSupport.class);
 
             JavaRDD<String> lines = sc.textFile(inputFilePath);
 
-            JavaPairRDD<Integer, STBEvent> stbEventRDD = sc.hadoopRDD((JobConf) job.getConfiguration(), null, Integer.class, STBEvent.class);
-
-            lines.map(new PairFunction<String, Integer, STBEvent>() {
+            JavaPairRDD<Void, STBEvent> stbEventRDD = lines.map(new PairFunction<String, Void, STBEvent>() {
                 @Override
-                public Tuple2<Integer, STBEvent> call(String s) throws Exception {
+                public Tuple2<Void, STBEvent> call(String s) throws Exception {
 
-                    return new Tuple2<Integer, STBEvent>(counter.getAndAdd(1), EventParser.createEvent(s));
+                    return new Tuple2<Void, STBEvent>(v, EventParser.createEvent(s));
                 }
             });
 
             ParquetOutputFormat<STBEvent> dummy = new ParquetOutputFormat<STBEvent>();
 
-//            System.out.println("Events : " + events.count());
-//            events.saveAsTextFile(outputFilePath);
-            stbEventRDD.saveAsNewAPIHadoopFile(outputFilePath, Integer.class, STBEvent.class, dummy.getClass(), job.getConfiguration());
-//            // Save the RDD to a Parquet file
-//            events.saveAsNewAPIHadoopFile(args(2), classOf[Void], classOf[HotelTaxes],
-//                    classOf[ParquetOutputFormat[HotelTaxes]], job.getConfiguration)
+            stbEventRDD.saveAsNewAPIHadoopFile(outputFilePath, Void.class, STBEvent.class, dummy.getClass(), job.getConfiguration());
 
         } catch (Exception e) {
             e.printStackTrace();
